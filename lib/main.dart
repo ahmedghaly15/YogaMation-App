4b34c77e66d6a74aff903f8963503f621a9a821f
ahmed ghaly <ahmedghaly0767@gmail.com>
2023-05-10 21:03:01 +0300

    Change App Name & Logo

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,6 @@ import '/features/get_started/presentation/views/get_started_view.dart';
 
 void main() => runApp(const YogaAnimation());
 
-// TODO : change app logo to character in home view
-
 class YogaAnimation extends StatelessWidget {
   const YogaAnimation({super.key});
 
